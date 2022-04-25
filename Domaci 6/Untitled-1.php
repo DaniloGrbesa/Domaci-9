81d0448f7974432d5b9b244d0d8837b547bc325d
DaniloGrbesa <104026491+DaniloGrbesa@users.noreply.github.com>
2022-04-25 23:38:45 +0200

    Add files via upload

---
+++ Domaci 6/Untitled-1.php
@@ -0,0 +1,169 @@
+<?php
+
+?php>
+
+
+
+
+
+
+<!DOCTYPE html>
+<head>
+<link href="stilovi.css" rel="stylesheet">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+<header>
+  <div>
+   <img class="logo" src="../Domaci 6/TCGplayer-logo-primary@2x.png">
+   <nav role="navigation">
+    <div id="menuToggle">
+      
+      <input type="checkbox" />
+      
+      <span></span>
+      <span></span>
+      <span></span>
+      
+      <ul id="menu">
+        <a href="#"><li>Home</li></a>
+        <a href="#"><li>About</li></a>
+        <a href="#"><li>Info</li></a>
+        <a href="#"><li>Contact</li></a>
+      </ul>
+    </div>
+  </nav>
+</header>
+<div class="container">
+  <aside>
+    <div class="filter">
+      <h3>Option</h3>
+      <ul>
+        <li>
+          <input type="checkbox" name="" id="opcija1">
+          <label for="opcija1">Opcija1</label>
+        </li>
+        <li>
+          <input type="checkbox" name="" id="opcija1">
+          <label for="opcija1">Opcija1</label>
+        </li>
+        <li>
+          <input type="checkbox" name="" id="opcija1">
+          <label for="opcija1">Opcija1</label>
+        </li>
+      </ul>
+    </div>
+  </aside>
+  <main>
+     
+        
+ <article class="artikl">
+        <h2>Typhone</h2>
+        <div>
+          <img src="../Domaci 6/254e0403-67d8-4e73-8d89-c901ebeba49f.jpg" alt="artikl">
+          <div>
+            <p>155$</p>
+            <button>Add to cart</button>
+  
+          </div>
+        </div>
+ </article>
+ <article class="artikl">
+    <h2>Dark Ritual</h2>
+    <div>
+      <img src="../Domaci 6/254e0403-67d8-4e73-8d89-c901ebeba49f.jpg" alt="artikl">
+      <div>
+        <p>120$</p>
+        <button>Add to cart</button>
+
+      </div>
+    </div>
+</article>
+<article class="artikl">
+    <h2>Doomsday</h2>
+    <div>
+      <img src="../Domaci 6/254e0403-67d8-4e73-8d89-c901ebeba49f.jpg" alt="artikl">
+      <div>
+        <p>450$</p>
+        <button>Add to cart</button>
+
+      </div>
+    </div>
+</article>
+<article class="artikl">
+    <h2>Village rites</h2>
+    <div>
+      <img src="../Domaci 6/254e0403-67d8-4e73-8d89-c901ebeba49f.jpg" alt="artikl">
+      <div>
+        <p>330$</p>
+        <button>Add to cart</button>
+
+      </div>
+    </div>
+</article>
+<article class="artikl">
+    <h2>Opt</h2>
+    <div>
+      <img src="../Domaci 6/254e0403-67d8-4e73-8d89-c901ebeba49f.jpg" alt="artikl">
+      <div>
+        <p>120$</p>
+        <button>Add to cart</button>
+
+      </div>
+    </div>
+</article>
+<article class="artikl">
+    <h2>Glass</h2>
+    <div>
+      <img src="../Domaci 6/254e0403-67d8-4e73-8d89-c901ebeba49f.jpg" alt="artikl">
+      <div>
+        <p>90$</p>
+        <button>Add to cart</button>
+
+      </div>
+    </div>
+</article>
+<article class="artikl">
+    <h2>Negate</h2>
+    <div>
+      <img src="../Domaci 6/254e0403-67d8-4e73-8d89-c901ebeba49f.jpg" alt="artikl">
+      <div>
+        <p>70$</p>
+        <button>Add to cart</button>
+
+      </div>
+    </div>
+</article>
+<article class="artikl">
+    <h2>CounterSpell</h2>
+    <div>
+      <img src="../Domaci 6/254e0403-67d8-4e73-8d89-c901ebeba49f.jpg" alt="artikl">
+      <div>
+        <p>1500$</p>
+        <button>Add to cart</button>
+
+      </div>
+    </div>
+</article>
+<article class="artikl">
+    <h2>Evil Elf</h2>
+    <div>
+      <img src="../Domaci 6/254e0403-67d8-4e73-8d89-c901ebeba49f.jpg" alt="artikl">
+      <div>
+        <p>50$</p>
+        <button>Add to cart</button>
+
+      </div>
+    </div>
+</article>
+
+   
+  </main>
+</div>
+
+    <footer>
+        <a href=""><img src="https://img.icons8.com/ios/50/000000/circled-up-2.png"/ width="50px"></a>
+    </footer>
+
+</body>
+</html>
